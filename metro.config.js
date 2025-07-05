e9c1bdd70c754c460368ff0d394a89e3c66d62b9
benmneb <62878664+benmneb@users.noreply.github.com>
2025-07-05 15:56:10 +0800

    ✨ Persist storage
    use indexedDb on web for best iOS PWA performance

---
+++ metro.config.js
@@ -0,0 +1,11 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config')
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname)
+
+// Needed for zustand "Uncaught SyntaxError: import.meta may only appear in a module"
+// See: https://github.com/pmndrs/zustand/discussions/1967
+config.resolver.unstable_conditionNames = ['browser', 'require', 'react-native']
+
+module.exports = config
--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,26 @@
+import { Platform } from 'react-native'
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
+
+function getPersistentStorage() {
+  if (Platform.OS === 'web') {
+    const { get, set, del } = require('idb-keyval')
+    return {
+      getItem: (name: string) => get(name),
+      setItem: (name: string, value: string) => set(name, value),
+      removeItem: (name: string) => del(name),
+    }
+  } else {
+    const AsyncStorage =
+      require('@react-native-async-storage/async-storage').default
+    return {
+      getItem: (name: string) => AsyncStorage.getItem(name),
+      setItem: (name: string, value: string) =>
+        AsyncStorage.setItem(name, value),
+      removeItem: (name: string) => AsyncStorage.removeItem(name),
+    }
+  }
+}
 
 export type DawnTypes = 'astro' | 'nautical' | 'civil'
 
@@ -7,7 +29,15 @@ interface State {
   setDawnTime: (dawnTime: State['dawnTime']) => void
 }
 
-export const useStore = create<State>()((set) => ({
-  dawnTime: 'nautical',
-  setDawnTime: (dawnTime) => set({ dawnTime }),
-}))
+export const useStore = create<State>()(
+  persist(
+    (set, get) => ({
+      dawnTime: 'nautical',
+      setDawnTime: (dawnTime) => set({ dawnTime }),
+    }),
+    {
+      name: 'alms-time-storage',
+      storage: createJSONStorage(getPersistentStorage),
+    }
+  )
+)
